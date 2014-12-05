b98747a25c3f2c6f5fb0190b8b17b6db7389a95d
Meghan Hartman <meghan.hartman@nyu.edu>
2014-12-05 17:01:42 -0500

    created user scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :users
+
   resources :cards
 
   get 'pages/landing_page'
--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141205180445) do
+ActiveRecord::Schema.define(version: 20141205215948) do
 
   create_table "cards", force: true do |t|
     t.integer  "serial"
     t.string   "name"
     t.integer  "zip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
+    t.string   "login"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
