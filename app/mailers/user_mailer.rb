3013cc0abb7cc71022d2acf5249231a3dc9a2854
Meghan Hartman <meghan.hartman@nyu.edu>
2014-12-05 18:08:10 -0500

    usermail bug fix

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,7 +1,7 @@
 class UserMailer < ActionMailer::Base
   default from: "app32246337@heroku.com"
 
-  def thanks_email(@user)
+  def thanks_email(user)
   	@user = user
   	@url = 'https://fierce-reaches-4548.herokuapp.com/'
   	mail(to: @user.email, subject: 'Thank you for registering')
