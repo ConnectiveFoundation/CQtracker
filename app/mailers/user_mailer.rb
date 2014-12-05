ccc0d73b7f59c7babf249727719ec442763ecc21
Meghan Hartman <meghan.hartman@nyu.edu>
2014-12-05 17:57:04 -0500

    attempt to configure usermailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
 class UserMailer < ActionMailer::Base
   default from: "app32246337@heroku.com"
 
-  def thanks_email(card)
-  	@card = card
+  def thanks_email(@user)
+  	@user = user
   	@url = 'https://fierce-reaches-4548.herokuapp.com/'
-  	mail(to: @card.email, subject: 'Thank you for tracking a card')
+  	mail(to: @user.email, subject: 'Thank you for registering')
   end
 end
