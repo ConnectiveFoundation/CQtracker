4e437d5e41dba19725a97fe8e51f6364a59b64d7
Meghan Hartman <meghan.hartman@nyu.edu>
2014-12-16 10:21:59 -0500

    remove search from location model

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,8 +1,4 @@
 class Location < ActiveRecord::Base
 	geocoded_by :address
 	after_validation :geocode, :if => :address_changed?
-
-	algoliasearch do
-	    attribute :address
-	  end
 end
--- config/initializers/algoliasearch.rb
+++ config/initializers/algoliasearch.rb
@@ -1,1 +1,1 @@
-AlgoliaSearch.configuration = { application_id: 'IKMJ9EN278', api_key: '985ab9fa3a9a887433c0fa59431d335c' }
+AlgoliaSearch.configuration = { application_id: 'IKMJ9EN278', api_key: 'ce7b1aaedfb0819964c1514d5b143faf' }
