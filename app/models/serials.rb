e127a92eb508ca7cb90094f9d62458241c7239e5
Meghan Hartman <meghan.hartman@nyu.edu>
2014-12-19 13:20:38 -0500

    bug sort

--- app/models/serials.rb
+++ app/models/serials.rb
@@ -1,5 +1,5 @@
 class Serials < ActiveRecord::Base
 	has_many :locations, dependent: :destroy
 	validates :serial_number, presence: true, uniqueness: true
-	#default_scope order("#{self.locations}.serial_number ASC")
+	default_scope order("#{self.locations}.serial_number ASC")
 end
