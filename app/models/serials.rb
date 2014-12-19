71601fb3be6aac6b66413608ff4d57b8801dc3c8
Meghan Hartman <meghan.hartman@nyu.edu>
2014-12-19 13:09:19 -0500

    fix sort

--- app/models/serials.rb
+++ app/models/serials.rb
@@ -1,5 +1,5 @@
 class Serials < ActiveRecord::Base
 	has_many :locations, dependent: :destroy
 	validates :serial_number, presence: true, uniqueness: true
-	default_scope order('serial_number ASC')
+	Serials.order(:serial_number)
 end
