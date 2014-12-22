b41c7d7c6e8e3b83976ee46dc12bfbd5ebc9c934
Meghan Hartman <meghan.hartman@nyu.edu>
2014-12-22 10:50:18 -0500

    maps not working

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,8 +3,6 @@ class Location < ActiveRecord::Base
 	geocoded_by :address
 	after_validation :geocode, :if => :address_changed?
 
-	#default_scope  { order(:serial_number => :asc) }
-
 	# It returns the locations whose serial numbers match the query
 	def self.search(query)
 	# where(:serial_number, query) -> This would return an exact match of the query
