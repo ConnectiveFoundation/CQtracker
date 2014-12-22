9edfa061b131665fa635fcbf2130f413ed143625
Meghan Hartman <meghan.hartman@nyu.edu>
2014-12-22 10:19:36 -0500

    bug

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,9 +1,7 @@
 class Location < ActiveRecord::Base
 	belongs_to :serial
 	geocoded_by :address
-	after_validation :geocode, :if => :address_changed?
-
-	#default_scope  { order(:serial_number => :asc) }
+	#after_validation :geocode, :if => :address_changed?
 
 	# It returns the locations whose serial numbers match the query
 	def self.search(query)
