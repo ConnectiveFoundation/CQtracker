6189e3dc1d255523a41f967e191fdb39206c6f29
Meghan Hartman <meghan.hartman@nyu.edu>
2015-01-02 15:03:12 -0500

    location controller bug

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,7 +5,7 @@ class LocationsController < ApplicationController
   # GET /locations.json
   def index
     @locations = Location.all
-    @locations = Location.paginate(:page => params[:page], :per_page => 10)
+    #@locations = Location.paginate(:page => params[:page], :per_page => 10)
     @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
       marker.lat location.latitude
       marker.lng location.longitude
