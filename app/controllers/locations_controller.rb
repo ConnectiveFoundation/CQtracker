fd729529edc87f6461be0765e8638b7c7b0433e1
Meghan Hartman <meghan.hartman@nyu.edu>
2015-01-23 17:57:35 -0500

    attempt to alter map based on search

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -5,6 +5,7 @@ class LocationsController < ApplicationController
   # GET /locations.json
   def index
     @locations = Location.all
+    @locations = Location.search(params[:search])
     @hash = Gmaps4rails.build_markers(@locations) do |location, marker|
       marker.lat location.latitude
       marker.lng location.longitude
